b411d1542604b2d2e7cde36f683035a7f4c0882a
Lukasz <lukasz.grela@gmail.com>
2016-09-20 11:47:38 +0100

    PokeCell class

---
+++ s10-pokedex/View/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  s10-pokedex
+//
+//  Created by Lukasz Grela on 20.09.2016.
+//  Copyright © 2016 Commelius Solutions Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class PokeCell: UICollectionViewCell {
+
+}
