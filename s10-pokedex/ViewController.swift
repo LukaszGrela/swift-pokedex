978cde7167041229e53139ef6b19681c15978851
Lukasz <lukasz.grela@gmail.com>
2016-09-20 10:34:36 +0100

    Initial Commit

---
+++ s10-pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  s10-pokedex
+//
+//  Created by Lukasz Grela on 20.09.2016.
+//  Copyright © 2016 Commelius Solutions Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
