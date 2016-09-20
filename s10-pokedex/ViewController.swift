dc124d0a30b7846f82637f960f7dbeca1e6dd2ca
Lukasz <lukasz.grela@gmail.com>
2016-09-20 12:30:16 +0100

    Attaching the collection view, with some dummy data

--- s10-pokedex/ViewController.swift
+++ s10-pokedex/ViewController.swift
@@ -8,15 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collection:UICollectionView!
+    
+    
+    var pokemons = [Pokemon]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        for i in 1...30 {
+            pokemons.append(Pokemon(name: "poke-\(i)", id: i))
+        }
         
-        //print(Pokemon(name: "electabuzz", id: 125))
+        collection.delegate = self
+        collection.dataSource = self
     }
 
 
+    
+    
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        //
+        var cell:PokeCell
+        if let _cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell {
+            cell = _cell
+        
+        } else {
+            cell = PokeCell()
+        }
+        
+        
+        cell.configureCell(pokemons[indexPath.row])
+        
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        //
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //
+        return pokemons.count
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSize(width: 105, height: 105)
+    }
 }
 
